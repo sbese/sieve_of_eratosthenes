f035960d89b5103a5ef72bbd64c12cf6ec26d29e
sbese <sergbuks2@gmail.com>
2020-01-09 02:37:30 +0300

    add input field

--- test.js
+++ test.js
@@ -2,20 +2,21 @@ var example1 = new Vue({
     el: '#example-1',
     data: 
     {
-      numbers : 100
+      numbers : 15,
+      cells_in_row : 6
     },
     computed:
     {
         rows : function()
         {
-            return Math.ceil(this.numbers / 10)
+            return Math.ceil(this.numbers / this.cells_in_row)
         },
         last_row_ceil : function()
         {
-            l_r_c = this.numbers % 10
+            l_r_c = this.numbers % this.cells_in_row
             if(l_r_c == 0)
             {
-                l_r_c = 10
+                l_r_c = this.cells_in_row
             }
             return l_r_c
         }
