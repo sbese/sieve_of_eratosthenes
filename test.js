5d9028189246aa9ea326eb4f6e9dcc39f7be2054
sbese <sergbuks2@gmail.com>
2020-01-09 06:23:28 +0300

    it almost works

--- test.js
+++ test.js
@@ -2,8 +2,13 @@ var example1 = new Vue({
     el: '#example-1',
     data: 
     {
-      numbers : 15,
-      cells_in_row : 6
+      numbers : 0,
+      cells_in_row : 10,
+      prime_numbers : []
+    },
+    created: function()
+    {
+        this.numbers = 100;
     },
     computed:
     {
@@ -19,6 +24,53 @@ var example1 = new Vue({
                 l_r_c = this.cells_in_row
             }
             return l_r_c
+        }    
+    },
+    watch:
+    {
+        numbers: function(numbers)
+        {
+            this.prime_numbers = []
+            for(let i = 0; i<=numbers; i++)
+            {
+                this.prime_numbers.push(null)
+            }
+        }
+    },
+    methods:
+    {
+        get_prime(i)
+        {
+            if (this.prime_numbers[i] === null)
+            {
+                this.prime_numbers.splice(i, 1, true)
+                for(j=i**2; j<=this.numbers; j+=i)
+                {
+                    this.prime_numbers.splice(j, 1, false)
+                }
+            }
+            if(i<=this.numbers)
+            {
+                if(this.prime_numbers[i] === true && i**2<=this.numbers)
+                {
+                    setTimeout(this.get_prime,1500, ++i)    
+                }
+                else if(this.prime_numbers[i] === true && i<=this.numbers)
+                {
+                    setTimeout(this.get_prime,300, ++i)    
+                }
+                else
+                {
+                    setTimeout(this.get_prime,100, ++i)
+                }
+            }
+        },
+        get_prime_numbers: function()
+        {
+
+            this.prime_numbers.splice(1, 1, false)
+            this.prime_numbers.splice(0, 1, false)
+            setTimeout(this.get_prime,1500, 2)
         }
     }
   })
